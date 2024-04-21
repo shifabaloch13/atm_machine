ebf65b3d7e5265c1649fddaffb86d31d930b7209
shifabaloch13 <167632110+shifabaloch13@users.noreply.github.com>
2024-04-21 12:58:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 10000; //dollar
+let mypin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === mypin)
+    console.log("correct pin code!!!");
+let operationans = await inquirer.prompt([
+    {
+        name: "opperation",
+        message: "please select option",
+        type: "list",
+        choices: ["withdraw,", "check balance"]
+    }
+]);
+console.log(operationans);
+if (operationans.operation === "withdraw") {
+    let amountans = await inquirer.prompt([
+        {
+            name: "amount",
+            message: "enter your amount",
+            type: "number"
+        }
+    ]);
+    myBalance -= amountans.amount;
+    console.log("your remaining balance is: " + myBalance);
+}
+else if (operationans.operation === "check balance") {
+    console.log("your balance is:" + myBalance);
+}
+else {
+    console.log("incorrect pin number");
+}
